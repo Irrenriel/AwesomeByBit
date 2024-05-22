dc8872d9baba68a318114236e668e5c5ce516e42
Irrenriel <yuronsuper@gmail.com>
2024-05-22 21:36:26 +0300

    Added setup file.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='AwesomeByBit',
     version='0.1.0',
+    description='Awesome async lib for working with ByBit!',
+    author='Irrenriel',
     install_requirements=[
         'aiohttp == 3.9.3',
         'pydantic == 2.5.3'
