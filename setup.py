bdf885a87e6656dc8b935a4044128ad411602984
Irrenriel <yuronsuper@gmail.com>
2024-05-22 22:09:58 +0300

    Added setup file.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='AwesomeByBit',
+    name='awesomebybit',
     version='0.1.0',
     description='Awesome async lib for working with ByBit!',
     author='Irrenriel',
+    license='MIT',
     url='https://github.com/Irrenriel/AwesomeByBit',
     install_requires=[
         'aiohttp == 3.9.3',
         'pydantic == 2.5.3'
     ],
-    packages=find_packages(where='AwesomeByBit'),
-    package_dir={"": "AwesomeByBit"},
+    packages=find_packages(),
     python_requires=">=3.12"
 )
